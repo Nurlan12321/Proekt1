7b4a4993331c11b1c6dd14d97128e78df6051e8b
Нурлан <@->
2022-11-25 22:54:31 +0300

    Добавьте файлы проекта.

---
+++ Proekt/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Proekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(284, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(249, 239);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Действующие элементы";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(128, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Отмена";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(250, 294);
+            this.treeView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 335);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Proekt/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+
+namespace Proekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //create root
+            var root = new TreeNode() { Text = "C", Tag = "c:\\" };
+            treeView1.Nodes.Add(root);
+            Build(root);
+            root.Expand();
+        }
+        private void Build(TreeNode parent)
+        {
+            var path = parent.Tag as string;
+            parent.Nodes.Clear();
+
+            try
+            {
+                //create dirs
+                foreach (var dir in Directory.GetDirectories(path))
+                {
+                    var node = new TreeNode(Path.GetFileName(dir), new[] { new TreeNode("...") }) { Tag = dir };
+                    parent.Nodes.Add(node);
+                }
+
+                //create files
+                foreach (var file in Directory.GetFiles(path))
+                {
+                    var node = new TreeNode(Path.GetFileName(file), 1, 1) { Tag = file };
+                    parent.Nodes.Add(node);
+                }
+            }
+            catch
+            {
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Проверка, что поле поиска не пустое.
+            if (textBox1.Text.Length > 0)
+            {
+                //Проверка, что поиск не тот же.
+                if (LastSearchText != textBox1.Text)
+                {
+                    //Очищаем переменную,
+                    //хранящую найденные элементы.
+                    CurrentNodeMatches.Clear();
+                    //Сохраняем текущий поиск.
+                    LastSearchText = textBox1.Text;
+                    //Запускаем метод поиска, с передачей 
+                    //в качестве параметров, текст для поиска и 
+                    //с какого элемента начинать поиск.
+                    SearchNodes(textBox1.Text, treeView1.Nodes[0]);
+                }
+
+                //Проверка количества найденных элементов.
+                if (CurrentNodeMatches.Count > 0)
+                {
+                    //Проходимся по найденным элементам.
+                    for (int i = 0; i < CurrentNodeMatches.Count; i++)
+                    {
+                        //Берем текущий элемент
+                        TreeNode selectedNode = CurrentNodeMatches[i];
+                        //Задаем узел дерева, который в текущий момент будет выбран в элементе
+                        //управления иерархического представления.
+                        this.treeView1.SelectedNode = selectedNode;
+                        //Разворачиваем выбранный узел.
+                        this.treeView1.SelectedNode.Expand();
+                        //Задаем цвет фона для выбранного узла.
+                        this.treeView1.SelectedNode.BackColor = Color.FromArgb(0xCC, 0xFF, 0x00);
+                        //Активируем элемент управления.
+                        this.treeView1.Select();
+                    }
+                }
+            }
+        }
+
+        //Переменная для хранения найденных элементов.
+        private List<TreeNode> CurrentNodeMatches = new List<TreeNode>();
+        //Переменная для хранения данных последнего поиска.
+        private string LastSearchText;
+
+        //Метод поиска, принимающий в качестве входных параметров,
+        //текст для поиска и узел с которого надо начинать поиск.
+        private void SearchNodes(string SearchText, TreeNode StartNode)
+        {
+            while (StartNode != null)
+            {
+                //Проверяем значение поиска, равно ли оно данному экземпляру.
+                //Если получено значение true, то параметр value встречается
+                //в строке, иначе — false.
+                if (StartNode.Text.ToLower().Contains(SearchText.ToLower()))
+                {
+                    //Добавляем элемент в список найденных.
+                    CurrentNodeMatches.Add(StartNode);
+                };
+
+                //Если в текущем узле элементы не закончились,
+                //то запускам метод поиска.
+                if (StartNode.Nodes.Count != 0)
+                {
+                    SearchNodes(SearchText, StartNode.Nodes[0]);
+                };
+                //Берем следующий узел для поиска.
+                StartNode = StartNode.NextNode;
+            };
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            Build(e.Node);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Очищаем поле поиска.
+            textBox1.Text = "";
+
+            //Проверяем количество найденных элементов.
+            if (CurrentNodeMatches.Count > 0)
+            {
+                //Проходимся по найденным элементам.
+                for (int i = 0; i < CurrentNodeMatches.Count; i++)
+                {
+                    //Берем текущий элемент.
+                    TreeNode selectedNode = CurrentNodeMatches[i];
+                    //Задаем узел дерева, который в текущий
+                    //момент будет выбран в элементе
+                    //управления иерархического представления.
+                    this.treeView1.SelectedNode = selectedNode;
+                    //Сворачиваем выбранный узел.
+                    this.treeView1.SelectedNode.Collapse();
+                    //Задаем цвет фона по умолчанию, для выбранного узла.
+                    this.treeView1.SelectedNode.BackColor = treeView1.BackColor;
+                    //Активируем элемент управления.
+                    this.treeView1.Select();
+                }
+            }
+        }
+
+        private void treeView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            var proc = new System.Diagnostics.Process();
+            proc.StartInfo.FileName = $"C:\\";
+            proc.StartInfo.UseShellExecute = true;
+            proc.Start();
+        }
+    }
+}
